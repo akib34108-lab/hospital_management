3ca636f21ee8e768f2d4eaacef373cb4ac152726
Akibul Islam <akib34108@gmail.com>
2026-07-26 11:57:52 +0600

    first commit

---
+++ component/footer.php
@@ -0,0 +1,14 @@
+   <div class="sidebar-overlay" data-reff=""></div>
+    <script src="assets/assets/js/jquery-3.2.1.min.js"></script>
+	<script src="assets/assets/js/popper.min.js"></script>
+    <script src="assets/assets/js/bootstrap.min.js"></script>
+    <script src="assets/assets/js/jquery.slimscroll.js"></script>
+    <script src="assets/assets/js/Chart.bundle.js"></script>
+    <script src="assets/assets/js/chart.js"></script>
+    <script src="assets/assets/js/app.js"></script>
+
+</body>
+
+
+
+</html>
