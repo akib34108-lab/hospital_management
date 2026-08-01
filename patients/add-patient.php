261ae7aa7940017eed83ecd257a1b1dcd90a0a16
Akibul Islam <akib34108@gmail.com>
2026-08-01 12:25:25 +0600

    patient add

--- patients/add-patient.php
+++ patients/add-patient.php
@@ -1,24 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-
-<!-- add-patient24:06-->
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
-    <title>Preclinic - Medical & Hospital - Bootstrap 4 Admin Template</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/select2.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-datetimepicker.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <!--[if lt IE 9]>
-		<script src="assets/js/html5shiv.min.js"></script>
-		<script src="assets/js/respond.min.js"></script>
-	<![endif]-->
-</head>
-
+<?php require_once "../component/header.php"; ?>
+<!-- Sidebar Start -->
+<?php require_once "../component/sidebar.php"; ?>
+<!-- Sidebar End -->
 <body>
     <div class="main-wrapper">
         <div class="header">
