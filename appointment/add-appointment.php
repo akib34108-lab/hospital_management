ed0ec7a941c0dc8e8a821ac6385f7991affe7da6
azadkhondoker <azadkhondoker444@gmail.com>
2026-08-08 13:04:48 +0600

    edit and deletue patient update

--- appointment/add-appointment.php
+++ appointment/add-appointment.php
@@ -2,6 +2,8 @@
 <!-- Sidebar Start -->
 <?php require_once "../component/sidebar.php"; ?>
 <!-- Sidebar End -->
+
+
         <div class="page-wrapper">
             <div class="content">
                 <div class="row">
