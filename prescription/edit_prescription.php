4cd77db0f2de32156aa87edff6a928481c98fb3a
WDPF_IDB <WDPF@nh.ctg>
2026-08-08 10:16:08 +0600

    prescription done

--- prescription/edit_prescription.php
+++ prescription/edit_prescription.php
@@ -37,6 +37,25 @@
                     WHERE `id`='$id'";
 
             if($conn->query($sql)){
+                
+                // 1. Puran sob medicine delete
+                $conn->query("DELETE FROM prescription_medicines WHERE prescription_id=$id");
+
+                // 2. Notun sob medicine insert
+                if(isset($_POST['medicine_name'])){
+                    foreach($_POST['medicine_name'] as $key => $med){
+                        if(!empty($med)){
+                            $dosage = $conn->real_escape_string($_POST['dosage'][$key]);
+                            $freq = $conn->real_escape_string($_POST['frequency'][$key]);
+                            $duration = $conn->real_escape_string($_POST['duration'][$key]);
+                            $inst = $conn->real_escape_string($_POST['instructions'][$key]);
+                            
+                            $conn->query("INSERT INTO prescription_medicines (prescription_id, medicine_name, dosage, frequency, duration, instructions) 
+                            VALUES ($id, '$med', '$dosage', '$freq', '$duration', '$inst')");
+                        }
+                    }
+                }
+
                 echo "<script>alert('Prescription Updated Successfully'); window.location='prescription_list.php';</script>";
                 exit;
             } else {
@@ -125,7 +144,60 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <!-- MEDICINE SECTION START -->
+                        <hr>
+                        <h5>Medicines</h5>
+                        <button type="button" class="btn btn-sm btn-success mb-2" id="addRow">+ Add Medicine</button>
+                        <table class="table table-bordered" id="medicineTable">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th width="25%">Medicine Name</th>
+                                    <th width="12%">Dosage</th>
+                                    <th width="15%">Frequency</th>
+                                    <th width="12%">Duration</th>
+                                    <th>Instructions</th>
+                                    <th width="8%">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                $mid = $conn->query("SELECT * FROM prescription_medicines WHERE prescription_id=$id");
+                                if($mid->num_rows > 0){
+                                    while($m = $mid->fetch_assoc()){ ?>
+                                    <tr>
+                                        <td><input type="text" name="medicine_name[]" value="<?=$m['medicine_name']?>" class="form-control" required></td>
+                                        <td><input type="text" name="dosage[]" value="<?=$m['dosage']?>" class="form-control" placeholder="1+0+1"></td>
+                                        <td><input type="text" name="frequency[]" value="<?=$m['frequency']?>" class="form-control" placeholder="After Meal"></td>
+                                        <td><input type="text" name="duration[]" value="<?=$m['duration']?>" class="form-control" placeholder="7 Days"></td>
+                                        <td><input type="text" name="instructions[]" value="<?=$m['instructions']?>" class="form-control"></td>
+                                        <td><button type="button" class="btn btn-danger btn-sm removeRow">X</button></td>
+                                    </tr>
+                                    <?php } 
+                                } // while + if sesh ?>
+                            </tbody>
+                        </table>
+
+                        <script>
+                        document.getElementById('addRow').onclick = function(){
+                            let row = `<tr>
+                                <td><input type="text" name="medicine_name[]" class="form-control" required></td>
+                                <td><input type="text" name="dosage[]" class="form-control" placeholder="1+0+1"></td>
+                                <td><input type="text" name="frequency[]" class="form-control" placeholder="After Meal"></td>
+                                <td><input type="text" name="duration[]" class="form-control" placeholder="7 Days"></td>
+                                <td><input type="text" name="instructions[]" class="form-control"></td>
+                                <td><button type="button" class="btn btn-danger btn-sm removeRow">X</button></td>
+                            </tr>`;
+                            document.querySelector('#medicineTable tbody').insertAdjacentHTML('beforeend', row);
+                        }
+                        document.addEventListener('click', function(e){
+                            if(e.target.classList.contains('removeRow')){
+                                e.target.closest('tr').remove();
+                            }
+                        })
+                        </script>
+                        <!-- MEDICINE SECTION END -->
+
+                        <div class="form-group row mt-3">
                             <div class="col-md-10 offset-md-2">
                                 <button type="submit" name="update" class="btn btn-primary">Update Prescription</button>
                             </div>
--- prescription/prescription.php
+++ prescription/prescription.php
@@ -1,7 +1,6 @@
 <?php require_once "../component/header.php";?>
 <?php require_once "../component/sidebar.php";?>
 <?php
-    session_start();
     $base_url = "http://localhost/shifa/";
     require_once  ($_SERVER['DOCUMENT_ROOT'] . "/shifa/crud/crud_class.php");
     $crud = new crud_class();
@@ -26,12 +25,7 @@ if(isset($_GET['id'])){
         echo "<div class='alert alert-danger'>Prescription not found</div>";
         exit;
     }
-
-    // JSON ke array te convert
-    $medicines = [];
-    if(!empty($row['medicines'])){
-        $medicines = json_decode($row['medicines']);
-    }
+    
 } else {
     echo "<div class='alert alert-danger'>Invalid ID</div>";
     exit;
@@ -43,7 +37,7 @@ if(isset($_GET['id'])){
         <div class="row">
             <div class="col-sm-12">
                 <div class="card-box">
-                    <div class="text-right mb-3">
+                    <div class="text-right mb-3 no-print">
                         <a href="prescription_list.php" class="btn btn-secondary"><i class="fa fa-arrow-left"></i> Back</a>
                         <a href="javascript:window.print()" class="btn btn-primary"><i class="fa fa-print"></i> Print</a>
                     </div>
@@ -71,22 +65,35 @@ if(isset($_GET['id'])){
                     <div class="table-responsive">
                         <table class="table table-bordered table-striped">
                             <thead class="thead-light">
-                                <tr><th>#</th><th>Medicine Name</th><th>Dosage</th><th>Frequency</th><th>Duration</th><th>Instructions</th></tr>
+                                <tr>
+                                    <th width="5%">#</th>
+                                    <th width="25%">Medicine Name</th>
+                                    <th width="10%">Dosage</th>
+                                    <th width="15%">Frequency</th>
+                                    <th width="10%">Duration</th>
+                                    <th>Instructions</th>
+                                </tr>
                             </thead>
                             <tbody>
                             <?php
                             $i=1;
-                            if(!empty($medicines) && is_array($medicines)){
-                                foreach($medicines as $med){ ?>
+                            // Notun table theke medicine gula tana
+                            $med_sql = "SELECT * FROM prescription_medicines WHERE prescription_id='$pres_id' ORDER BY id ASC";
+                            $med_result = mysqli_query($conn, $med_sql);
+
+                            if(mysqli_num_rows($med_result) > 0){
+                                while($med = mysqli_fetch_assoc($med_result)){ ?>
                                 <tr>
                                     <td><?php echo $i++;?></td>
-                                    <td><?php echo $med->medicationName;?></td>
-                                    <td><?php echo $med->dosage;?></td>
-                                    <td><?php echo $med->frequency;?></td>
-                                    <td><?php echo $med->duration;?></td>
-                                    <td><?php echo $med->instructions;?></td>
+                                    <td><?php echo $med['medicine_name'];?></td>
+                                    <td><?php echo $med['dosage'];?></td>
+                                    <td><?php echo $med['frequency'];?></td>
+                                    <td><?php echo $med['duration'];?></td>
+                                    <td><?php echo $med['instructions'];?></td>
                                 </tr>
-                            <?php }} else { echo "<tr><td colspan='6' class='text-center'>No Medicine Added</td></tr>"; } ?>
+                            <?php }} else { 
+                                echo "<tr><td colspan='6' class='text-center'>No Medicine Added</td></tr>"; 
+                            } ?>
                             </tbody>
                         </table>
                     </div>
@@ -100,5 +107,48 @@ if(isset($_GET['id'])){
         </div>
     </div>
 </div>
+
+<style>
+@media print {
+    /* 1. Sidebar, Header, Button hide */
+    .sidebar, 
+    .header, 
+    .page-title, 
+    .breadcrumb, 
+    .no-print, 
+    nav, 
+    footer {
+        display: none !important;
+    }
+
+    /* 2. Pura width nibe */
+    .page-wrapper {
+        margin: 0 !important;
+        padding: 0 !important;
+        width: 100% !important;
+    }
+    
+    .content {
+        margin: 0 !important;
+        padding: 20px !important;
+    }
+
+    /* 3. Card er border/shadow bad */
+    .card-box {
+        box-shadow: none !important;
+        border: none !important;
+        padding: 0 !important;
+    }
+
+    body {
+        background: white !important;
+        -webkit-print-color-adjust: exact;
+    }
+    
+    table {
+        page-break-inside: avoid;
+    }
+}
+</style>
 
 <?php require_once "../component/footer.php"?>
