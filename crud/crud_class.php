500c7963a9bc1e1a98fe1da9ce6902cda2882395
Akibul Islam <akib34108@gmail.com>
2026-08-24 11:51:53 +0600

    update

--- crud/crud_class.php
+++ crud/crud_class.php
@@ -1,16 +1,26 @@
 <?php
 
 class crud_class{
-    
-    private $host = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $database = "shifa";
+
+    private $host;
+    private $username;
+    private $password;
+    private $database;
     public $conn;
 
-    public function __construct(){
+    public function __construct($host = "localhost", $username = "root", $password = "", $database = "shifa")
+    {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+        $this->connect();
+    }
+
+    private function connect()
+    {
         $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
-        if($this->conn->connect_error){
+        if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
         }
     }
